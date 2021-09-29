3b0d1bf2e12a000a7f3d3a85bc5df53bb3f1a8d5
LucyMoon <81845462+LucyMoon@users.noreply.github.com>
2021-09-29 20:50:42 +0900

    login

---
+++ app/src/main/java/kr/hs/narsha_ls/LoginLayout.kt
@@ -0,0 +1,12 @@
+package kr.hs.narsha_ls
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_layout)
+    }
+
+}
