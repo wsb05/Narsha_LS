0eb4a71bd2d18fdd5ba43fdec1690ccfa5365dfa
LucyMoon <81845462+LucyMoon@users.noreply.github.com>
2021-09-29 20:41:15 +0900

    Register layout

---
+++ app/src/main/java/kr/hs/narsha_ls/RegisterLayout.kt
@@ -0,0 +1,11 @@
+package kr.hs.narsha_ls
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_layout)
+    }
+}
