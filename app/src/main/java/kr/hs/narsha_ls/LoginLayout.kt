05553c448ce777b3eab4522ac53793daa56f4f1d
ssl82 <ssl82@naver.com>
2021-10-05 19:57:11 +0900

    버전수정

--- app/src/main/java/kr/hs/narsha_ls/LoginLayout.kt
+++ app/src/main/java/kr/hs/narsha_ls/LoginLayout.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 class LoginLayout : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_layout)
+        setContentView(R.layout.activity_register_layout)
     }
 
 }
