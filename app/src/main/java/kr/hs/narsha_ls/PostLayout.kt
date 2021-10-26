7a4d3882d0bd6b37d844ede211cad535eaf0a552
LucyMoon <81845462+LucyMoon@users.noreply.github.com>
2021-10-26 20:47:58 +0900

    commit

--- app/src/main/java/kr/hs/narsha_ls/PostLayout.kt
+++ app/src/main/java/kr/hs/narsha_ls/PostLayout.kt
@@ -1,11 +1,18 @@
 package kr.hs.narsha_ls
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class PostLayout : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post_layout)
+
+        val writingBtn = findViewById<Button>(R.id.writing_btn)
+        writingBtn.setOnClickListener{
+            startActivity(Intent(this, Writingscreenactivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/kr/hs/narsha_ls/Writingscreenactivity.kt
@@ -0,0 +1,11 @@
+package kr.hs.narsha_ls
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Writingscreenactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.writingscreen)
+    }
+}
